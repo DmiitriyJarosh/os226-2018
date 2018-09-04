6ae55a6ddb865c1345769fcbf80b789891f41867
Dmitriy <dimajarosh@gmail.com>
2018-09-04 19:00:42 +0300

    implempted some string functions

--- libc/string.c
+++ libc/string.c
@@ -2,32 +2,74 @@
 #include <stddef.h>
 #include <string.h>
 
-static inline __attribute__((noreturn)) void IMPL_ME() {
-	extern void hal_halt(void);
-	hal_halt();
-	while(1);
-}
-
 int strcmp(const char *s1, const char *s2) {
-	IMPL_ME();
+  int i = 0;
+ 	while (s1[i]) {
+        	if (s1[i] > s2[i]) return s1[i] - s2[i];
+        	if (s1[i] < s2[i]) return s1[i] - s2[i];
+        	i++;
+    	}
+    	return 0;
 }
 
 size_t strlen(const char *s) {
-	IMPL_ME();
+  size_t n = 0;
+    	while (s[n] != '\0') {
+        	n++;
+    	}
+    	return n;
 }
 
 char *strchr(const char *s, int c) {
-	IMPL_ME();
+  	while (1) {
+        	if (*s == c) return (char*)s;
+        	if (*s == '\0') return NULL;
+        	s++;
+	}
 }
 
 size_t strspn(const char *str, const char *accept) {
-	IMPL_ME();
+  	size_t n;
+    	const char* p;
+    	for (n = 0; *str; str++, n++) {
+        	for (p = accept; *p && *p != *str; p++){};
+        	if (!*p) break;
+    	}
+    	return n;
 }
 
 size_t strcspn(const char *str, const char *reject) {
-	IMPL_ME();
+  	size_t n;
+    	const char* p;
+    	for (n = 0; *str; str++, n++) {
+        	for (p = reject; *p && *p != *str; p++){};
+        	if (*p) break;
+    	}
+    	return n;
 }
 
 char *strtok_r(char *str, const char *delim, char **saveptr) {
-	IMPL_ME();
+  	char *res;
+  	char *oldptr;
+  	if (!str) {
+    		str = *saveptr;
+    		if (!str) {
+      			return NULL;
+    		}
+  	}
+  	while (!strcspn(str, delim) && *str) {
+    		str++;
+  	}
+  	if (!*str) {
+    		return NULL;
+  	}
+  	res = str;
+  	oldptr = str + strcspn(str, delim);
+  	if (*oldptr) {
+    		*oldptr = '\0';
+    		*saveptr = oldptr + 1;
+  	} else {
+    		*saveptr = oldptr;
+  	}
+  	return res;
 }
